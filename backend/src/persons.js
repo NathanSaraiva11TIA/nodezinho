ef16d25bf77a4af2760bd0bd603bd5659711b17d
NathanSaraiva11TIA <161879316+NathanSaraiva11TIA@users.noreply.github.com>
2025-10-19 22:15:58 -0300

    Initial commit

---
+++ backend/src/persons.js
@@ -0,0 +1,31 @@
+const persons = [
+    {
+        name: "Luiza Barreto",
+        email: "luizinha inquieta@gmail.com",
+        age: "17",
+        nickname: "Luluquentona"
+    },
+
+    {
+        name: "Matheus Murija",
+        email: "matheustigrao@gmail.com",
+        age: "16",
+        nickname: "tigrão",
+    },
+
+    {
+        name: "João Macedo",
+        email: "joaoemaria@gmail.com",
+        age: "16",
+        nickname: "Pau mandado"
+    },
+
+    {
+        name: "David",
+        email: "davizinhogameplay@gmail.com",
+        age: "16",
+        nickname: "pk"
+    }
+]
+
+export { persons }
